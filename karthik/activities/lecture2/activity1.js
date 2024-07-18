ebe4b6acb5960e9f485f775c5b8901e3be05e8cb
karthikp <karthikp@ece.ubc.ca>
2024-07-18 10:39:23 -0700

    Modified to reflect activity

--- karthik/activities/lecture2/activity1.js
+++ karthik/activities/lecture2/activity1.js
@@ -1,1 +1,26 @@
-document.writeln("World!");
+// Solution to activity 1
+//
+
+function getRandomInt(min, max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor( Math.random() * (max - min)) + min;
+}
+
+function randomarray(n, min, max) {
+	// Write randomArray code here
+
+}
+
+function sortArray(arr) {
+	// Write sortArray code here
+
+}
+
+for (var i=0; i<10; ++i) {
+	console.log( getRandomInt(0,9) );
+}
+
+
+
+
