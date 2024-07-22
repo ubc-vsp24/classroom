220124f301656e6145a7611a3ca028b8926d00fb
karthikp <karthikp@ece.ubc.ca>
2024-07-22 10:44:38 -0700

    Added class activties and examples

---
+++ karthik/activities/lecture4/interval.js
@@ -0,0 +1,9 @@
+// Solution to the class activity using setItnerval
+
+var invokeTimes = function(func, num, time) {
+    // to implement
+};
+
+invokeTimes( function(count) { 
+      console.log("Hello " + count);
+   }, 10, 1000 );
