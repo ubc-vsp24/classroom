5b15729534e1ad5c93d3317ad82c49985571c47a
karthikp <karthikp@ece.ubc.ca>
2024-07-23 10:17:11 -0700

    Added code examples

---
+++ karthik/activities/lecture5/changeImage.js
@@ -0,0 +1,10 @@
+var changeImages = function(id, offset) {
+	// Write your solution here 
+
+}
+
+window.onload = function() {
+	setInterval( function() { 
+		changeImages("parent", 1);
+	}, 3000);
+};
