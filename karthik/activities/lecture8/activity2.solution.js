c6527d293b0e2f00307cf20270e9ec733c415cba
karthikp <karthikp@ece.ubc.ca>
2024-07-31 15:46:19 -0700

    Bug fix to setup promise one at a time

--- karthik/activities/lecture8/activity2.solution.js
+++ karthik/activities/lecture8/activity2.solution.js
@@ -1,17 +1,19 @@
 // Function to invoke foo n times
 function invoke(foo, n, delay){
+      console.log("Calling invoke...", n);
       var p = new Promise((resolve, reject)=> {
 	    var callFoo = function() { 
 		   foo(n); 
 		   resolve(); 
 		   console.log("Resolved promise " + n);
-	    }; 
-        setTimeout( callFoo, delay * n);
+	    };
+	// console.log("Setting up promise...");
+        setTimeout( callFoo, delay );
      });
 
      // Recursive call to invoke (if n>0)
     if (n > 0)		
-		return p.then( invoke( foo, n-1, delay) );
+		return p.then( () =>  invoke( foo, n-1, delay) );
     else
 		return p;
 }
@@ -22,6 +24,5 @@ var fn = function(i) {
 
 var fp = invoke( fn, 10, 1000);
 
-fp.then("All promises resolved");
 console.log("Done setting up promises");
 
